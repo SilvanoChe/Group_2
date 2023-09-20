4327ec86f209e590b40f0552ca02652cdae5134c
Jovie Jurac <joviejurac22@gmail.com>
2023-09-20 19:59:57 +0800

    Jovie has committed

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ console.log ("Kenet");
 console.log("Angelica")
 console.log("Nica");
 console.log("Apple");
-console.log("Jovie");
+console.log("Jovie Jurac");
+console.log("Francis");
+console.log("Bryan");
+
+
 
