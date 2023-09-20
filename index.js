024c81cb9898b4cfdbe5887f04e17901a99531d9
patricia <22104920@usc.edu.ph>
2023-09-20 20:06:48 +0800

    Patricia has committed

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-console.log("Cherry");
+console.log("CherryRose");
 console.log("Reyver");
+console.log("Curt");
+console.log ("Kenet");
+console.log("Angelica")
+console.log("Nica");
+console.log("Apple");
+console.log("Francis");
+console.log("Bryan");
+console.log("Jerlon");
+console.log("Patricia");
+
+
